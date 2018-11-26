953a82b2ba773697c90d1faefe5c4c3c1a59fbef
Christian Kienle <christian.kienle@sap.com>
2018-11-26 17:55:14 +0100

    small fixes

--- src/docs/DocsRouter/DocsRouter.ts
+++ src/docs/DocsRouter/DocsRouter.ts
@@ -15,13 +15,13 @@ export const DocsRouter = new Router({
       name: 'start',
       path: '/start',
       component: StaticContent,
-      props: { html: require('@/docs/static-pages/start.md') },
+      props: { html: require('@/docs/static-pages/start.md').default },
     },
     {
       name: 'guide-new-component',
       path: '/guide/new-component',
       component: StaticContent,
-      props: { html: require('@/../NEW_COMPONENT/NEW_COMPONENT.md') },
+      props: { html: require('@/../NEW_COMPONENT/NEW_COMPONENT.md').default },
     },
     {
       path: '/example/:slug',
