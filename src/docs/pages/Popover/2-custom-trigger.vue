9b5d6eef28049b0d86eeaf288f3161cfb5d754ee
Christian Kienle <kienle.christian@icloud.com>
2018-12-09 02:15:10 +0100

    fixes popover positioning in example

--- src/docs/pages/Popover/2-custom-trigger.vue
+++ src/docs/pages/Popover/2-custom-trigger.vue
@@ -1,6 +1,8 @@
 <title>Popover with custom Trigger</title>
 
 <template>
+<!-- div is needed: otherwise the popover will not be positioned correctly -->
+<div>
   <FdPopover>
     <FdButton styling="emphasized" type="positive" slot="control">Custom Popover Trigger Control</FdButton>
     <FdMenuItem>Option 1</FdMenuItem>
@@ -8,4 +10,5 @@
     <FdMenuItem>Option 3</FdMenuItem>
     <FdMenuItem>Option 4</FdMenuItem>
   </FdPopover>
+</div>
 </template>
