20b4568e09ddb4904f50c574262d7f48a3ffb200
Damian Lion Maring <damian.lion.maring@sap.com>
2018-12-13 10:43:17 +0100

    initial commit of product-switcher

---
+++ src/components/Layout/ShellBar/ShellBarProductSwitcher.tsx
@@ -0,0 +1,32 @@
+import { Component } from 'vue-property-decorator';
+import { componentName } from '@/util';
+import { Api } from '@/api';
+import { Button, Popover } from '@/components';
+import TsxComponent from '@/vue-tsx';
+
+@Component({ name: componentName('ShellBarProductSwitcher') })
+@Api.Component('Shell Bar Product Switcher')
+@Api.defaultSlot('Product Items (FdShellBarProductSwitcherItem)')
+@Api.slot('control', 'Popover Control (optional). Defaults to a specially configured FdIdentifier.')
+export class ShellBarProductSwitcher extends TsxComponent<{}> {
+  public render() {
+    const control = this.$slots.control;
+    return (
+      <div class='fd-product-switcher'>
+        <Popover placement='right'>
+          <div class='fd-user-menu__control' slot='control'>
+            {control}
+            {!control && <Button styling='light' class='fd-button--shell' icon='grid'/>}
+          </div>
+          <div class='fd-product-switcher__body'>
+            <nav>
+              <ul>
+                {this.$slots.default}
+              </ul>
+            </nav>
+          </div>
+        </Popover>
+      </div>
+    );
+  }
+}
