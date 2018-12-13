35769f6a7c95e96fd3e771e4d3cba08a828950f9
Damian Lion Maring <damian.lion.maring@sap.com>
2018-12-13 14:26:10 +0100

    support multiple variants of product items

--- src/docs/pages/Shell Bar/default.vue
+++ src/docs/pages/Shell Bar/default.vue
@@ -24,41 +24,32 @@
             </FdShellBarAction>
             <FdShellBarAction>
               <FdShellBarProductSwitcher>
-                <FdShellBarProductSwitcherItem href="#" src="/images/products/01.png">
-                  Fiori Home
+                <FdShellBarProductSwitcherItem href="#" src="/images/products/01.png" title="Fiori Home"/>
+                <FdShellBarProductSwitcherItem href="#" src="/images/products/02.png" title="S/4 HANA Cloud"/>
+                <FdShellBarProductSwitcherItem href="#" src="/images/products/03.png" title="Analytics Cloud"/>
+                <FdShellBarProductSwitcherItem :to="{path:'/test'}" src="/images/products/04.png" title="Ariba"/>
+                <FdShellBarProductSwitcherItem :to="{path:'/'}" src="/images/products/05.png" title="SuccessFactors"/>
+                <FdShellBarProductSwitcherItem :to="{path:'/'}" src="/images/products/06.png" title="Commerce Cloud"/>
+                <FdShellBarProductSwitcherItem src="/images/products/07.png" title="Gigya"/>
+                <FdShellBarProductSwitcherItem src="/images/products/08.png" title="Callidus Cloud"/>
+                <FdShellBarProductSwitcherItem src="/images/products/09.png" title="Fieldglass"/>
+                <FdShellBarProductSwitcherItem>
+                  <router-link :to="{path:'/'}">
+                    <FdShellBarProductSwitcherItemImg src="/images/products/10.png"/>
+                    <FdShellBarProductSwitcherItemTitle>Concur</FdShellBarProductSwitcherItemTitle>
+                  </router-link>
                 </FdShellBarProductSwitcherItem>
-                <FdShellBarProductSwitcherItem href="#" src="/images/products/02.png">
-                  S/4 HANA Cloud
+                <FdShellBarProductSwitcherItem src="/images/products/11.png">
+                  <router-link :to="{path:'/'}">
+                    <FdShellBarProductSwitcherItemImg src="/images/products/11.png"/>
+                    <FdShellBarProductSwitcherItemTitle>Cloud for Customer</FdShellBarProductSwitcherItemTitle>
+                  </router-link>
                 </FdShellBarProductSwitcherItem>
-                <FdShellBarProductSwitcherItem href="#" src="/images/products/03.png">
-                  Analytics Cloud
-                </FdShellBarProductSwitcherItem>
-                <FdShellBarProductSwitcherItem :to="{path:'/'}" src="/images/products/04.png">
-                  Ariba
-                </FdShellBarProductSwitcherItem>
-                <FdShellBarProductSwitcherItem :to="{path:'/'}" src="/images/products/05.png">
-                  SuccessFactors
-                </FdShellBarProductSwitcherItem>
-                <FdShellBarProductSwitcherItem :to="{path:'/'}" src="/images/products/06.png">
-                  Commerce Cloud
-                </FdShellBarProductSwitcherItem>
-                <FdShellBarProductSwitcherItem :to="{path:'/'}" src="/images/products/07.png">
-                  Gigya
-                </FdShellBarProductSwitcherItem>
-                <FdShellBarProductSwitcherItem :to="{path:'/'}" src="/images/products/08.png">
-                  Callidus Cloud
-                </FdShellBarProductSwitcherItem>
-                <FdShellBarProductSwitcherItem :to="{path:'/'}" src="/images/products/09.png">
-                  Fieldglass
-                </FdShellBarProductSwitcherItem>
-                <FdShellBarProductSwitcherItem :to="{path:'/'}" src="/images/products/10.png">
-                  Concur
-                </FdShellBarProductSwitcherItem>
-                <FdShellBarProductSwitcherItem :to="{path:'/'}" src="/images/products/11.png">
-                  Cloud for Customer
-                </FdShellBarProductSwitcherItem>
-                <FdShellBarProductSwitcherItem :to="{path:'/'}" src="/images/products/12.png">
-                  Cloud Portal
+                <FdShellBarProductSwitcherItem>
+                  <router-link :to="{path:'/'}">
+                    <FdShellBarProductSwitcherItemImg src="/images/products/12.png"/>
+                    <FdShellBarProductSwitcherItemTitle>Cloud Portal</FdShellBarProductSwitcherItemTitle>
+                  </router-link>
                 </FdShellBarProductSwitcherItem>
               </FdShellBarProductSwitcher>
             </FdShellBarAction>
--- src/docs/pages/Shell Bar/index.ts
+++ src/docs/pages/Shell Bar/index.ts
@@ -1,6 +1,6 @@
 import { ExampleCollectionFunction } from '../types';
-import { ShellBarProduct, ShellBarGroup, ShellBarLogo, ShellBarActions, ShellBarAction, ShellBarUserMenu, ShellBarProductSwitcher, ShellBar, ShellBarProductSwitcherItem } from '@/components';
+import { ShellBarProduct, ShellBarGroup, ShellBarLogo, ShellBarActions, ShellBarAction, ShellBarUserMenu, ShellBarProductSwitcher, ShellBar, ShellBarProductSwitcherItem, ShellBarProductSwitcherItemImg, ShellBarProductSwitcherItemTitle } from '@/components';
 
 export const plugin: ExampleCollectionFunction = ({ Shell }) => {
-  return { experimental: true, icon: 'header', relatedComponents: [ShellBar, ShellBarGroup, ShellBarProduct, ShellBarLogo, ShellBarActions, ShellBarAction, ShellBarUserMenu, ShellBarProductSwitcher, ShellBarProductSwitcherItem] };
+  return { experimental: true, icon: 'header', relatedComponents: [ShellBar, ShellBarGroup, ShellBarProduct, ShellBarLogo, ShellBarActions, ShellBarAction, ShellBarUserMenu, ShellBarProductSwitcher, ShellBarProductSwitcherItem, ShellBarProductSwitcherItemImg, ShellBarProductSwitcherItemTitle] };
 };
