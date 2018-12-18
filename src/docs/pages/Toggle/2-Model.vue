5d68eea06e08db75ed08dbf9a74bb2b4171cd411
Christian Kienle <kienle.christian@icloud.com>
2018-12-18 23:23:12 +0100

    fixes vmodel for popover and toggle (#71)

---
+++ src/docs/pages/Toggle/2-Model.vue
@@ -0,0 +1,22 @@
+<title>Toggle and `v-model`</title>
+<docs>`FdToggle` supports `v-model`. This allows you to bind an `FdToggle` to a `boolean` property of your component. The example below uses `v-model` to control the visbibility of a popover.</docs>
+<template>
+  <div>
+    <FdToggle v-model="isOn" :label="String(isOn)" />
+    <FdToggle v-model="isOn" :label="String(isOn)" />
+    <FdButton @click="isOn = !isOn">Toggle the Toggles</FdButton>
+    <FdPopover v-model="isOn">
+      <div slot="body" v-padding:large><h1>hi</h1></div>
+    </FdPopover>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOn: false,
+    }
+  }
+}
+</script>
