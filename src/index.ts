c61ee38e1450cd24a9a1e094a602a627882ba54a
ChristianKienle <kienle.christian@icloud.com>
2019-01-14 17:47:36 +0100

    removes vue router and makes everything work again

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import * as components from '@/components';
 import Directives from '@/directives';
 import { PluginFunction } from 'vue';
 import { PluginAPIOptions, PluginAPI } from '@/lib/plugin';
-import VueRouter from 'vue-router';
 
 type Options = {
   log: {
@@ -12,7 +11,6 @@ type Options = {
 } & object;
 
 const Plugin: PluginFunction<Options> = (vue, options?: PluginAPIOptions) => {
-  vue.use(VueRouter);
   vue.use(Directives);
   const api = new PluginAPI(options);
   components.plugin().install(vue, api);
