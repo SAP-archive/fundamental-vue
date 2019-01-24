76ff1dda55418317e59c4562e8580115a7aaf3ff
I072513 <r.biscoveanu@sap.com>
2019-01-24 10:31:39 +0000

    change required label display

--- src/components/Form/FormLabel.tsx
+++ src/components/Form/FormLabel.tsx
@@ -1,6 +1,6 @@
-import { Inject } from 'vue-property-decorator';
-import { Component, DefaultSlot, Prop, Base } from '@/core';
-import { FORM_ITEM_ID_KEY } from './FormItem/Types';
+import {Inject} from 'vue-property-decorator';
+import {Component, DefaultSlot, Prop, Base} from '@/core';
+import {FORM_ITEM_ID_KEY} from './FormItem/Types';
 
 interface Props {
   for?: string | null;
@@ -8,22 +8,26 @@ interface Props {
 }
 
 @Component('FormLabel')
-@DefaultSlot('Contents of the label: For non-inline elements simply use text which will become the text displayed by the label. For inline elements use text alongside with any elements that form your input control.')
-export class FormLabel extends Base<Props> {
-  @Inject(FORM_ITEM_ID_KEY) public itemId!: string;
+@DefaultSlot('Contents of the label: For non-inline elements simply use text which will become' +
+    ' the text displayed by the label. For inline elements use text alongside with an' +
+    'y elements that form your input control.')
+export class FormLabel extends Base < Props > {
+  @Inject(FORM_ITEM_ID_KEY)public itemId !: string;
 
-  private get for(): string {
-    return this.itemId;
-  }
-
-  @Prop('whether a value is required (adds a *)', { default: false, type: Boolean })
-  public required!: boolean;
+  private get for(): string {return this.itemId;}
+  
+  @Prop('whether a value is required (adds a *)', {
+    default: false,
+    type: Boolean
+  })
+  public required !: boolean;
 
   public render() {
-    return <label staticClass='fd-form__label' class={this.classes} for={this.for}>{this.$slots.default}</label>;
-  }
-
-  private get classes() {
-    return { 'is-required': this.required };
+    return <label
+      staticClass='fd-form__label'
+      for={this.for}
+      aria-required={this.required}>{this.$slots.default}{this.required
+        ? '*'
+        : ''}</label>;
   }
 }
