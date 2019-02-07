b54080af4401c44d933e2710f23015444dc6fbfc
Killian M <killian.merrins@sap.com>
2019-02-07 18:51:59 +0000

    Table Test

--- src/components/Popover/__tests__/Popover.test.ts
+++ src/components/Popover/__tests__/Popover.test.ts
@@ -1,20 +1,39 @@
-import { expect } from 'chai';
+import { expect, assert } from 'chai';
 import { shallowMount } from '@vue/test-utils';
 import Popover from '../Popover.vue';
-import { MenuItem } from '../../Menu';
+import MenuList from '../../Menu/MenuList.vue';
 
 describe('Popover', () => {
-  it('renders menu item when passed to the default slots', () => {
-    const wrapper = shallowMount(Popover, {
-      template: `<MenuItem>M1</MenuItem>`,
-    });
-    expect(wrapper.find(MenuItem).text()).to.include('M1');
+    it('renders menu list as default slot', () => {
+      const wrapper = shallowMount(Popover, {
+        slots: {
+          default: 'Hi',
+        },
+      });
+      expect(wrapper.find(MenuList).exists()).to.equal(true)
   });
+  it('renders custom content in body slot', () => {
+    const wrapper = shallowMount(Popover, {
+      slots: {
+        body: '<div>Hi</div>',
+      },
+    });
+    expect(wrapper.find('div').text()).to.include('Hi');
+  });
+  it('clicking control slot content emits visible event', () => {
+    const wrapper = shallowMount(Popover, {
+      slots: {
+        control: '<p>Hi</p>',
+      },
+    });
+    const emitVisibleArray = wrapper.emitted().visible;
+    //Prior to click, wrapper.emitted().visible = [ [false] ]
+    assert.lengthOf(emitVisibleArray, 1);
+    wrapper.find('p').trigger('click')
+    //After click, wrapper.emitted().visible = [ [false], [true] ]
+    assert.lengthOf(emitVisibleArray, 2);
+    expect(wrapper.emitted().visible[1][0]).to.equal(true)
 
-  it('renders custom body', () => {
-    const wrapper = shallowMount(Popover, {
-      template: `<div slot='body'>hell world</div>`,
-    });
-    expect(wrapper.find('div').text()).to.include('hell world');
   });
-});
+})
+
