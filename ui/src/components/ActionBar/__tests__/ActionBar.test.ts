8133cd50153546223ba35a9542283fccf37360df
Suwarno Ong <suwarno.ong@sap.com>
2019-03-05 15:34:53 +0800

    feat: add tests for ActionBar component

---
+++ ui/src/components/ActionBar/__tests__/ActionBar.test.ts
@@ -0,0 +1,32 @@
+import { mount } from "@vue/test-utils";
+import ActionBar from "../ActionBar.vue";
+
+describe("ActionBar", () => {
+  it("renders correctly", () => {
+    const wrapper = mount(ActionBar, {
+      propsData: {
+        title: "Page Title",
+        description: "Action bar descrtiption",
+      },
+      slots: {
+        back: 'Back',
+        default: 'Actions'
+      }
+    });
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it("renders without description", () => {
+    const wrapper = mount(ActionBar, {
+      propsData: {
+        title: "Page Title",
+      },
+      slots: {
+        back: 'Back',
+        default: 'Actions'
+      }
+    });
+    expect(wrapper.element).toMatchSnapshot();
+    expect(wrapper.find(".fd-action-bar__description").exists()).toBe(false);
+  });
+})
