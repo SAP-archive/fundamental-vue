41e73a1a83332a54e567ecbe538614336c2e23f1
Deepshikha Mohanta <deepshikha.mohanta6@gmail.com>
2019-03-07 11:54:35 +0530

    test cases added for status (#219)

---
+++ ui/src/components/Status/__tests__/Status.test.ts
@@ -0,0 +1,47 @@
+import { shallowMount } from "@vue/test-utils";
+import Status from '../Status.vue';
+import { assert } from "chai";
+
+describe("Status test scripts", () => {
+    test("Renders correctly", () => {
+        const statusIcon = "available";
+        const type = "success";
+        const status = shallowMount(Status,{
+            propsData : {
+                statusIcon, type
+            }
+        });
+        expect(status.element).toMatchSnapshot();
+    });
+
+    test("Default slot rendering test", () => {
+        const slotText = "Status Slot Text";
+        const status = shallowMount(Status, {
+            slots: {
+                default : slotText
+            }
+        });
+        expect(status.text()).toBe(slotText);
+    });
+
+    test("Status indicator with built in status icon test", () => {
+        const statusIcon = "offline";
+        const status = shallowMount(Status, {
+            propsData : {
+                statusIcon
+            }
+        });
+        assert(status.classes(`fd-status-label--${statusIcon}`));
+    });
+
+    test("Status indicator with semantic colors", () => {
+        const type = "warning";
+        const status = shallowMount(Status, {
+            propsData : {
+                type
+            }
+        });
+        assert(status.classes(`fd-status-label--${type}`));
+
+    });
+});
