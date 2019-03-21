8c98bfce8ca7043e3f44b124dc122b26c1a3dc86
Suwarno Ong <suwarno.ong@sap.com>
2019-03-21 23:17:33 +0800

    fix(action bar): adds slots for title and description

---
+++ docs/src/pages/Action Bar/1-title-slot.vue
@@ -0,0 +1,26 @@
+<title>Action bar with title and description as slots.</title>
+
+<template>
+  <FdActionBar title="Page Title" description="Action bar Description">
+    <template #title>
+      <FdIdentifier size="s" circle thumbnail url="/images/headshot-male.jpg" />
+      <span class="name">Matthew Spark</span>
+    </template>
+    <template #description>
+      <FdBadge pill type="success">UI/UX</FdBadge>
+      <FdBadge pill type="success">Developer</FdBadge>
+      <FdBadge pill type="success">VIP</FdBadge>
+      <FdBadge pill type="success">Star</FdBadge>
+    </template>
+    <FdButton slot="back" styling="light" compact icon="nav-back" />
+  </FdActionBar>
+</template>
+
+<style scoped>
+.name {
+  margin-left: 5px;
+}
+.fd-badge {
+  margin-right: 3px;
+}
+</style>
--- ui/src/components/ActionBar/__tests__/ActionBar.test.ts
+++ ui/src/components/ActionBar/__tests__/ActionBar.test.ts
@@ -1,23 +1,39 @@
-import { mount } from "@vue/test-utils";
+import { CreateElement, VNode } from "vue";
+import { shallowMount } from "@vue/test-utils";
 import ActionBar from "../ActionBar.vue";
 
 describe("ActionBar", () => {
+  const title = "Page Title";
+  const description = "Action bar description";
+  const back = "Back";
+  const actions = "Actions";
+
+  const wrapper = shallowMount(ActionBar, {
+    propsData: {
+      title,
+      description
+    },
+    slots: {
+      back,
+      default: actions
+    }
+  });
+
   it("renders correctly", () => {
-    const wrapper = mount(ActionBar, {
-      propsData: {
-        title: "Page Title",
-        description: "Action bar descrtiption"
-      },
-      slots: {
-        back: "Back",
-        default: "Actions"
-      }
-    });
     expect(wrapper.element).toMatchSnapshot();
   });
 
-  it("renders without description", () => {
-    const wrapper = mount(ActionBar, {
+  it("renders correct props and slots", () => {
+    expect(wrapper.find(".fd-action-bar__title").text()).toEqual(title);
+    expect(wrapper.find(".fd-action-bar__description").text()).toEqual(
+      description
+    );
+    expect(wrapper.find(".fd-action-bar__back").text()).toEqual(back);
+    expect(wrapper.find(".fd-action-bar__actions").text()).toEqual(actions);
+  });
+
+  describe("Without description", () => {
+    const wrapper = shallowMount(ActionBar, {
       propsData: {
         title: "Page Title"
       },
@@ -26,7 +42,57 @@ describe("ActionBar", () => {
         default: "Actions"
       }
     });
-    expect(wrapper.element).toMatchSnapshot();
-    expect(wrapper.find(".fd-action-bar__description").exists()).toBe(false);
+
+    it("renders correctly", () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it("does not have description", () => {
+      expect(wrapper.contains(".fd-action-bar__description")).toBe(false);
+    });
+  });
+
+  describe("Title as slot", () => {
+    const title = "Page Title (slot)";
+    const wrapper = shallowMount(ActionBar, {
+      slots: {
+        title: {
+          render(h: CreateElement): VNode {
+            return h("div", {}, title);
+          }
+        }
+      }
+    });
+
+    it("renders correctly", () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it("renders title as slot", () => {
+      expect(wrapper.find(".fd-action-bar__title div").text()).toBe(title);
+    });
+  });
+
+  describe("Description as slot", () => {
+    const description = "Page Description (slot)";
+    const wrapper = shallowMount(ActionBar, {
+      slots: {
+        description: {
+          render(h: CreateElement): VNode {
+            return h("div", {}, description);
+          }
+        }
+      }
+    });
+
+    it("renders correctly", () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it("renders title as slot", () => {
+      expect(wrapper.find(".fd-action-bar__description div").text()).toBe(
+        description
+      );
+    });
   });
 });
