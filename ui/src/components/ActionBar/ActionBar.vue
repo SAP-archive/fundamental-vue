9f0e378d38b953cdfce9e79cd389bd90beecc972
suwarno ong <48011859+suwarnoong@users.noreply.github.com>
2019-03-22 08:53:07 +0800

    fix(action bar): adds slots for title and description (#258)

--- ui/src/components/ActionBar/ActionBar.vue
+++ ui/src/components/ActionBar/ActionBar.vue
@@ -4,9 +4,15 @@
       <slot name="back" />
     </div>
     <div class="fd-action-bar__header">
-      <h1 class="fd-action-bar__title">{{ title }}</h1>
-      <p v-if="description != null" class="fd-action-bar__description">
-        {{ description }}
+      <h1 class="fd-action-bar__title">
+        <slot name="title">
+          {{ title }}
+        </slot>
+      </h1>
+      <p v-if="hasDescription" class="fd-action-bar__description">
+        <slot name="description">
+          {{ description }}
+        </slot>
       </p>
     </div>
     <div v-if="hasActions" class="fd-action-bar__actions">
@@ -20,10 +26,13 @@ import Vue from "vue";
 export default Vue.extend({
   name: "FdActionBar",
   props: {
-    title: { type: String, required: true },
+    title: { type: String, required: false },
     description: { type: String, default: null }
   },
   computed: {
+    hasDescription(): boolean {
+      return this.$slots.description != null || this.description != null;
+    },
     hasActions(): boolean {
       return this.$slots.default != null;
     },
