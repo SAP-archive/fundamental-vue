8abc4ecc4abc146b18ab927ec1ea4f2df83bd502
Christian Kienle <christian.kienle@sap.com>
2019-05-24 18:36:33 +0200

    feat: Menu Item Link can now be disabled

--- src/components/Menu/MenuLink.vue
+++ src/components/Menu/MenuLink.vue
@@ -1,16 +1,36 @@
 <template>
-  <a class="fd-menu__item" :class="classes"><slot /></a>
+  <a class="fd-menu__item" :class="classes" v-on="listeners" @click="click"
+    ><slot
+  /></a>
 </template>
 
 <script>
 export default {
   name: "FdMenuLink",
   props: {
-    selected: { type: Boolean, default: false }
+    selected: { type: Boolean, default: false },
+    disabled: { type: Boolean, default: false }
+  },
+  methods: {
+    click(event) {
+      if (this.disabled) {
+        event.preventDefault();
+        return;
+      }
+      this.$emit("click", event);
+    }
   },
   computed: {
+    listeners() {
+      const { click, ...others } = this.$listeners; // eslint-disable-line no-unused-vars
+      return others;
+    },
     classes() {
-      return this.selected ? "is-selected" : null;
+      return {
+        "is-selected": this.selected,
+        "is-disabled": this.disabled,
+        "fd-has-color-text-4": this.disabled
+      };
     }
   }
 };
--- src/components/Menu/__tests__/MenuLink.test.js
+++ src/components/Menu/__tests__/MenuLink.test.js
@@ -10,4 +10,26 @@ describe("MenuLink", () => {
       }).element
     ).toMatchSnapshot();
   });
+
+  it("can be disabled", () => {
+    const wrapper = mount({
+      template: `<fd-menu-link :disabled="disabled">title</fd-menu-link>`,
+      components: { FdMenuLink },
+      data() {
+        return {
+          disabled: false
+        };
+      }
+    });
+
+    const link = wrapper.find(FdMenuLink);
+    expect(link.emitted("click")).toBeUndefined();
+    wrapper.find("a").trigger("click");
+    const clicks = link.emitted("click");
+    expect(clicks.length).toBe(1);
+    wrapper.vm.disabled = true;
+    expect(wrapper.element).toMatchSnapshot();
+    wrapper.find("a").trigger("click");
+    expect(clicks.length).toBe(1);
+  });
 });
