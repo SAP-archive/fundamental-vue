357b8e76fd92914473a27c76c64a0a9ccb72de70
Christian Kienle <christian.kienle@sap.com>
2019-06-02 01:56:08 +0200

    feat: Added Dropdown component

---
+++ src/components/DropdownControl/DropdownControl.vue
@@ -0,0 +1,13 @@
+<template>
+  <fd-button class="fd-dropdown__control" v-on="$listeners" v-bind="$attrs"
+    ><slot
+  /></fd-button>
+</template>
+
+<script>
+import FdButton from "./../Button/Button.vue";
+export default {
+  name: "FdDropdownControl",
+  components: { FdButton }
+};
+</script>
