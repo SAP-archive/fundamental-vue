a8969b0952337e0b602dfb3d2072145174f1dd4e
Christian Kienle <christian.kienle@sap.com>
2019-06-07 21:56:02 +0200

    feat: VirtualizedList can now be resetted and is smarter.

--- demo/index.js
+++ demo/index.js
@@ -3,8 +3,10 @@ import router from "./router";
 import VueRouter from "vue-router";
 import FundamentalVue from "./../src";
 import FullscreenLayout from "./layouts/FullscreenLayout.vue";
+import VirtualScroller from "vue-virtual-scroller";
 import "./main.scss";
 
+Vue.use(VirtualScroller);
 Vue.use(VueRouter);
 Vue.use(FundamentalVue, {
   log: {
--- src/components/VirtualizedList/VirtualizedList.vue
+++ src/components/VirtualizedList/VirtualizedList.vue
@@ -1,17 +1,22 @@
 <template>
   <component
-    :is="dynamicScrollerComponent"
-    style="height: 100%;"
-    :min-item-size="minItemSize"
     key-field="fd__id"
+    style="height: 100%;"
+    :is="dynamicScrollerComponent"
+    :min-item-size="minItemSize"
     :items="normalizedItems"
     @scroll.native="handleOnScroll"
   >
-    <template #after>
-      <slot name="loading">
-        <fd-spinner v-if="isLoading" />
-      </slot>
+    <template v-slot:after>
+      <div ref="after">
+        <template v-if="isLoading">
+          <slot name="loading">
+            <fd-spinner v-if="isLoading" />
+          </slot>
+        </template>
+      </div>
     </template>
+
     <template v-slot="{ item, index, active }">
       <component
         :is="dynamicScrollerItemComponent"
@@ -23,7 +28,7 @@
         :class="rowClasses(item)"
         :size-dependencies="[item.title, item.subtitle]"
       >
-        <slot name="item" v-bind="{ item, index, active }" />
+        <slot name="item" v-bind="{ item, index, active }"></slot>
       </component>
     </template>
   </component>
@@ -45,18 +50,27 @@ export default {
   name: "FdVirtualizedList",
   components: { FdSpinner },
   props: {
+    // If set, FdVirtualizedList automatically requests more items. Assume that your list has space for 30 items to be displayed at the same time and after loading the initial batch there is still space left. If totalItemCount is set FdVirtualList automatically requests more items if there are more to fetch. This is repeated, until there is no space left. Of course the user can still load more items by scrolling to the bottom.
+    totalItemCount: {
+      type: Number,
+      default: null
+    },
+    // Minimal size of the items. Will be passed onto DynamicScroller.
     minItemSize: { type: Number, default: 30 },
+    // Under the hood FdVirtualizedList is using Vue's built in dynamic-component-mechanism to render components that belong to vue-virtual-scroller. In case you have customized the installation of vue-virtual-scroller use this prop to tell FdVirtualizedList about the DynamicScroller.
     dynamicScrollerComponent: {
       type: [Object, Function, String],
       default: "DynamicScroller"
     },
+    // See dynamicScrollerComponent for more information.
     dynamicScrollerItemComponent: {
       type: [Object, Function, String],
       default: "DynamicScrollerItem"
     },
+    // Items to be rendered by the virtualized list.
     items: { type: Array, default: () => [] },
-    loadMoreItems: { type: Function, default: null },
-    value: { type: Object, default: null } // selected item
+    // Function to be called when the list needs more items from you. This function is called with a callback parameter that you MUST call at some point with additional items.
+    loadMoreItems: { type: Function, default: null }
   },
   computed: {
     normalizedItems() {
@@ -66,12 +80,40 @@ export default {
       return this.state === "loading";
     }
   },
+  watch: {
+    items(newItems) {
+      this.items_ = newItems;
+    }
+  },
   mounted() {
     if (this.items.length === 0) {
       this.startToLoadMoreItems();
     }
+    this.$forceUpdate();
+  },
+  updated() {
+    requestAnimationFrame(() => {
+      this.loadMoreItemsIfNeeded();
+    });
   },
   methods: {
+    loadMoreItemsIfNeeded() {
+      const { totalItemCount, isLoading } = this;
+      const loadingIsPossible = totalItemCount != null && !isLoading;
+      if (!loadingIsPossible) {
+        return;
+      }
+      const isNeeded =
+        this.afterSlotIsVisible() && totalItemCount > this.items.length;
+      if (!isNeeded) {
+        return;
+      }
+      this.startToLoadMoreItems();
+    },
+    afterSlotIsVisible() {
+      const { $el, $refs } = this;
+      return $refs.after.getBoundingClientRect().top <= $el.scrollHeight;
+    },
     startToLoadMoreItems(event) {
       if (this.loadMoreItems != null) {
         this.state = "loading";
@@ -85,6 +127,10 @@ export default {
     acceptNewItems(newItems) {
       this.items_ = [...this.items_, ...newItems];
       this.state = "default";
+      this.$forceUpdate();
+      requestAnimationFrame(() => {
+        this.loadMoreItemsIfNeeded();
+      });
     },
     /** @param {Event} event */
     handleOnScroll(event) {
