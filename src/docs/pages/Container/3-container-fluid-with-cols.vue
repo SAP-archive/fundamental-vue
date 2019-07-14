0490749e2c61241b52f96b86ce0e770fc6436232
Christian Kienle <christian.kienle@sap.com>
2019-07-14 19:03:15 +0200

    feat: Container

---
+++ src/docs/pages/Container/3-container-fluid-with-cols.vue
@@ -0,0 +1,40 @@
+<title>Fluid with Cols</title>
+<template>
+  <div>
+    Columns:
+    <fd-button
+      :disabled="addDiabled"
+      @click="cols++"
+      styling="light"
+      icon="sys-add"
+    ></fd-button>
+    <fd-button
+      :disabled="removeDisabled"
+      @click="cols--"
+      styling="light"
+      icon="sys-minus"
+    ></fd-button>
+    <br />
+    <fd-container fluid>
+      <fd-col :span="1" v-for="col in cols" :key="`col-${col}`"
+        >{{ col }}. Column</fd-col
+      >
+    </fd-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { cols: 6 };
+  },
+  computed: {
+    addDiabled() {
+      return this.cols > 11;
+    },
+    removeDisabled() {
+      return this.cols < 1;
+    }
+  }
+};
+</script>
