096621a8eb8648d3164fc57450da11cf58055960
Christian Kienle <christian.kienle@sap.com>
2019-08-12 11:00:27 +0200

    fix: form examples

--- src/docs/content/examples/form/binding.vue
+++ src/docs/content/examples/form/binding.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <fd-field-set>
-      <fd-form-item label="First Name">
+      <fd-form-item>
+        <fd-form-label>First Name</fd-form-label>
         <fd-input v-model="firstName" placeholder="First Name" />
       </fd-form-item>
-      <fd-form-item label="First Name">
+      <fd-form-item>
+        <fd-form-label>First Name</fd-form-label>
         <fd-input v-model="firstName" placeholder="First Name" />
       </fd-form-item>
-      <fd-form-item label="First Name">
+      <fd-form-item>
+        <fd-form-label>First Name</fd-form-label>
         <fd-text-area v-model="firstName" placeholder="First Name" />
       </fd-form-item>
     </fd-field-set>
-    <fd-button @click="changeName">Set first name to 'Chris'</fd-button>
+    <fd-button @click="firstName = 'Max Mustermann'">Set first name to 'Max Mustermann'</fd-button>
     <br />
     <br />
     <br />
@@ -30,11 +33,6 @@ export default {
     return {
       firstName: "Chris"
     };
-  },
-  methods: {
-    changeName() {
-      this.firstName = "😇 Chris 😇";
-    }
   }
 };
 </script>
