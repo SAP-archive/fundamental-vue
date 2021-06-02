ecd16e699ddc3dcc5af029bdefd196887a3fedcb
Yury Betrozov <betrozov91@gmail.com>
2021-06-02 14:14:23 +0300

    chore: add commitlint, update changelog

--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -5,7 +5,7 @@ set -e
 git config --global user.email "fundamental@sap.com"
 git config --global user.name "fundamental-bot"
 
-yarn std-version -- --prerelease rc --no-verify
+npm run version.rc
 
 git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH" > /dev/null 2>&1;
 
